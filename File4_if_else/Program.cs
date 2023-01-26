28f6535ccd26b4429100981e909027655a0c956f
Sentinel Magnus <ekko2363@gmail.com>
2023-01-27 00:16:44 +0500

    added file if_else

---
+++ File4_if_else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "данил")
+{
+    Console.WriteLine("Wat de fuc do u want here?");
+}
+else
+{
+    Console.Write("Hi, bitch ");
+    Console.WriteLine(username);
+}
